2cf0e8150d7cc05471e9f84cfc8637430e94273c
AlanMestezk <alanmestezk98@gmail.com>
2026-08-14 20:24:02 -0300

    rules

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,8 +5,6 @@ import {Home} from "../pages/Home/Home";
 import { Login } from "../pages/Login/Login";
 import { Register } from "../pages/Register/Register";
 import { Dashboard } from "../pages/Dashboard/Dashboard";
-import { Transaction } from "../pages/Transactions/Transaction";
-import { Settings } from "../pages/Settings/Settings";
 import { Account } from "../pages/Acounts/Acount";
 import { ForgotPasswordForm } from "../components/ForgotPasswordForm/ForgotPasswordForm";
 import { NewExpenseForm } from "../components/Dashboard/Expenses/NewExpenseForm";
@@ -32,11 +30,7 @@ export const AppRoutes =()=> {
 
         <Route path="/dashboard" element={<Dashboard />} />
 
-        <Route path="/transactions" element={<Transaction />} />
-
         <Route path="/accounts" element={<Account />} />
-
-        <Route path="/settings" element={<Settings />} />
 
         <Route path="/forgot-password" element={<ForgotPasswordForm />} />
 
